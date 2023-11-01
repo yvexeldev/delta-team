8ffa0c699f58242578c4190c684d96f6835a8668
yvexeldev <azamabdusalomov@icloud.com>
2023-11-01 09:47:31 +0500

    project init

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <span>COMING SOON!</span>
+    <a href="https://deltateam_uz.t.me">@deltateam_uz</a>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="css" scoped>
+div {
+  width: 50rem;
+  height: 300px;
+  background-color: rgb(28, 28, 28);
+  margin: 10rem auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 1rem;
+  flex-direction: column;
+  gap: 2rem;
+  transition: 0.5s;
+}
+
+div:hover {
+  border: 1px solid white;
+  transform: scale(1.05);
+}
+
+span {
+  font-size: 3rem;
+  font-family: sans-serif;
+  font-weight: 700;
+  color: white;
+}
+
+a {
+  font-size: 2rem;
+  text-decoration: none;
+  color: white;
+  font-family: monospace;
+}
+</style>
